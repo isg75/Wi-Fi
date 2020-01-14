56bf321625bb6a0be2fed39fa4e3d5009dfc1b80
Ignacio <ignacio@ubiqum.com>
2020-01-14 20:07:42 +0100

    Seventh day of corrections

--- training.R
+++ training.R
@@ -2,47 +2,44 @@
 if ("pacman" %in% rownames(installed.packages()) == FALSE) {
   install.packages("pacman")
 } else {
-    pacman::p_load(readr,dplyr,magrittr,lubridate,ggplot2,plotly,reshape2,tidyr,caret)
+    pacman::p_load(readr,dplyr,magrittr,lubridate,ggplot2,plotly,reshape2,tidyr,
+                   caret,stringr)
 }
 
 
-# Load data
+#### Load data ###
 trainingData   <- read_csv("UBIQUM/MENTOR/DA119/Ferran Donoso/Wi-Fi/datasets/trainingData.csv")
 validationData <- read_csv("UBIQUM/MENTOR/DA119/Ferran Donoso/Wi-Fi/datasets/validationData.csv")
 
-
-# Pre processing ####
-# feature type
-# trainingData$FLOOR <- as.factor(trainingData$FLOOR)
-# trainingData$BUILDINGID <- as.factor(trainingData$BUILDINGID)
-# trainingData$SPACEID <- as.factor(trainingData$SPACEID)
-# trainingData$RELATIVEPOSITION <- as.factor(trainingData$RELATIVEPOSITION)
-# trainingData$USERID <- as.factor(trainingData$USERID)
-# trainingData$PHONEID <- as.factor(trainingData$PHONEID)
-# trainingData$TIMESTAMP <- as_datetime(trainingData$TIMESTAMP)
-# 
-# validationData$FLOOR <- as.factor(validationData$FLOOR)
-# validationData$BUILDINGID <- as.factor(validationData$BUILDINGID)
-# validationData$SPACEID <- as.factor(validationData$SPACEID)
-# validationData$RELATIVEPOSITION <- as.factor(validationData$RELATIVEPOSITION)
-# validationData$USERID <- as.factor(validationData$USERID)
-# validationData$PHONEID <- as.factor(validationData$PHONEID)
-# validationData$TIMESTAMP <- as_datetime(validationData$TIMESTAMP)
-
+#### Pre processing ####
 #### Ignacio: A better way to do it. 
-#### In this way you do two things in one:
+#### In this way you do several things in one:
 #### 1)-Sets the following variables to be categorical:
-#### "FLOOR","BUILDINGID","SPACEID","RELATIVEPOSITION","USERID","PHONEID"
+#### "FLOOR","BUILDINGID"
 #### 2)-Set the variable "TIMESTAMP" as DateTime.
+#### 3)-Remove columns "SPACEID","RELATIVEPOSITION", "USERID","PHONEID"
+#### as they are not used. Moreover, the model can't depend on such features
+#### as otherwise a new user will not be able to get his/her position as 
+#### there is no guarantee that a new USERID and PHONEID values being used
+#### in the model.
+#### 4)-Removing duplicated observations.
+#### 5)-Removing the "TIMESTAMP" as the model shouldn't depend on time.
 #### Inputs: the dataframe
 #### Output: the dataframe with the corresponding modifications.
 #### In this way you can prevent typing too much.
 #### You can apply the same function on training and validation sets.
 
 preproc <- function(df) {
-  cols <- c("FLOOR","BUILDINGID","SPACEID","RELATIVEPOSITION","USERID","PHONEID")
-  df %<>% mutate_at(cols, funs(factor(.)))
-  df$TIMESTAMP <- as_datetime(df$TIMESTAMP,origin="1970-01-01",tz="Europe/Madrid")
+  df$SPACEID <- NULL
+  df$RELATIVEPOSITION <- NULL
+  df$USERID <- NULL
+  df$PHONEID <- NULL
+  df$TIMESTAMP <- NULL
+  cols <- c("FLOOR","BUILDINGID")
+  df[,cols] <- lapply(df[,cols],as.factor)
+  if (sum(duplicated(df)) != 0) {
+    df <- df[!duplicated(df), ]
+  }
   return(df)
 }
 
@@ -61,17 +58,7 @@ combinedData <- bind_rows(trainingData, validationData)
 sum(is.na(trainingData), na.rm = TRUE)
 sum(is.na(validationData), na.rm = TRUE)
 
-#### Ignacio:
-# duplicated rows #637
-if (sum(duplicated(trainingData)) != 0) {
-  trainingData <- trainingData[!duplicated(trainingData), ]
-}
-
-if (sum(duplicated(validationData)) != 0) {
-  validationData <- validationData[!duplicated(validationData), ]
-}
-
-#### Ignacio: Data exploration.
+#### Ignacio: Data exploration. ####
 #### Those plots are not the best ones, as the dataset is 3D and the plots
 #### are 2D projections. Therefore, you can't see how the observations were 
 #### taken in each floor. Are all the floors evenly sampled? 
@@ -109,24 +96,31 @@ validationData[validationData == 100] <- NA
 #### the user will get NA for all the WAPS. 
 #### For this purpose, you can create a function to drop the columns for which all
 #### the values are NA's
+#### This function should be included in the preprocessing function. However it 
+#### has been deliberatelly left here for illustrative purposes.
 
-drop_allna_cols <- function(df) {
+drop_allna_rows <- function(df) {
   ind <- apply(df[,c(1:520)], 1, function(x) all(is.na(x)))
   df <- df[!ind,]
   return(df)
 }
 
 
-trainingData   <- drop_allna_cols(trainingData)
-validationData <- drop_allna_cols(validationData)
+trainingData   <- drop_allna_rows(trainingData)
+validationData <- drop_allna_rows(validationData)
 
+#### Feature selection: Filtering WAPS ####
 #### Ignacio: Explain better your purpose. You want to determine which waps
-#### were never used. In this case, all the rows of a particular WAP will  be
-#### NA.
+#### were never used. In this case, all the columns of a particular WAP will
+#### be NA.
 # columns that have the same value for every row
 # training data #55
 WAPs_train <- grep("WAP", names(trainingData), value=T)
 WAPs_val   <- grep("WAP", names(validationData), value=T)
+
+#### The not used WAPs doesn't need to be the same in both datasets. Therefore,
+#### if we want the most general model, we need to avoid working with any useless
+#### WAPS.
 
 not_used_waps_train <- apply(trainingData[,c(WAPs_train)],2,function(x) all(is.na(x)))
 not_used_waps_val   <- apply(validationData[,c(WAPs_val)],2,function(x) all(is.na(x)))
@@ -135,7 +129,8 @@ s <- which(not_used_waps_train==TRUE)
 p <- which(not_used_waps_val==TRUE)
 
 #### Ignacio: You can't use WAPs which were not available in any of the datasets
-#### observations
+#### observations.
+
 not_used_waps <- union(s,p)
 
 #### Ignacio: We free memory
@@ -154,11 +149,14 @@ WAPs <- grep("WAP", names(trainingData), value=T)
 #relocate very good and very bad signals
 dat1 <- melt(combinedData[, c(WAPs,"Set")])
 
-ggplot(dat1,aes(x=value,fill=Set)) +
+p0 <- ggplot(dat1,aes(x=value,fill=Set)) +
   geom_density(alpha = 0.2) +
   xlab("RSSI Signal value") +
   ggtitle("Densities of WAP signals for each dataset")
 
+p0
+
+#### Feature engenieering ###
 #### Ignacio: Obtaning the signal value for which the density is highest.
 #### Moreover, why do you pick the value of "a" based on the training set?
 a <- density(dat1$value, na.rm = TRUE)$x[which.max(density(dat1$value, na.rm = TRUE)$y)]
@@ -168,13 +166,17 @@ a <- density(dat1$value, na.rm = TRUE)$x[which.max(density(dat1$value, na.rm = T
 #### signal higher than -29 by -29. There is any reason for chosing this particular
 #### value? 
 
-#transform datasets
-replace_values <- function(x,value) {
-  ifelse(x < value,x<-value,ifelse(x > -29,x <- -29, x <- x))
+#transform datasets.
+#### We replace too small values by "NA" and values in the [-30,0] range to be
+#### -30 as values in this range are unrealistic.
+#### See: https://support.randomsolutions.nl/827069-Best-dBm-Values-for-Wifi
+
+replace_values <- function(x) {
+  ifelse(x < -90,x<- -90,ifelse(x > -30,x <- -30, x <- x))
 }
 
-trainingData[,c(WAPs)] <- mapply(replace_values,trainingData[,c(WAPs)],a)
-trainingData[,c(WAPs)] <- mapply(replace_values,trainingData[,c(WAPs)],a)
+trainingData[,c(WAPs)]   <- mapply(replace_values,trainingData[,c(WAPs)])
+validationData[,c(WAPs)] <- mapply(replace_values,validationData[,c(WAPs)])
 
 # create BUILDINGID_FLOOR feature
 build_floor <- function(df) {
@@ -227,8 +229,8 @@ multiple$Most_Frequent_location <- factor(multiple$Most_Frequent_location,
 #### to the Highest_loc_train dataframe with the prediction.
 Highest_loc_train$Prediction <- sapply(Highest_loc_train$WAP,function(x) multiple[which(multiple$WAP==x),]$Most_Frequent_location )
 
-#### Getting the confussion Matrix.
-confusionMatrix(trainingData$BUILDINGID_FLOOR, Highest_loc_train$Prediction)
+#### Getting the confussion Matrix of the model on the training set ####
+BFPred_cm <- confusionMatrix(trainingData$BUILDINGID_FLOOR, Highest_loc_train$Prediction)
 #### As it can be seen, the accuracy is pretty small: 0.53, while the kappa
 #### is 0.48. Therefore this model can be used as a benchmark, but not for
 #### production.
@@ -240,8 +242,21 @@ confusionMatrix(trainingData$BUILDINGID_FLOOR, Highest_loc_train$Prediction)
 #### For this purpose, on can add and extra column which a mismatch betwee the
 #### real location and the predicted coded in two colors: green = correct, red
 #### = incorrect.
-trainingData$BFPred <- sapply(Highest_loc_train,
-                              ifelse(BFLocation == Prediction,"Green","Red"))
+correctness <- function(x,y){
+  return(ifelse(x==y,"Green","Red"))
+}
+
+trainingData$Error_BFPred <- mapply(correctness,Highest_loc_train$BFLocation,Highest_loc_train$Prediction)
+trainingData$Error_BFPred <- as.factor(trainingData$Error_BFPred)
+
+# Plotting the locations where this simple model makes the mistakes.
+p1 <- plot_ly(trainingData, x = ~LONGITUDE, y = ~LATITUDE, z = ~FLOOR, color = ~Error_BFPred, colors = c('cyan', 'red'),
+             size = 1.2) %>% add_markers() %>%
+  layout(scene = list(xaxis = list(title = 'Longitude'),
+                      yaxis = list(title = 'Latitude'),
+                      zaxis = list(title = 'Floor')),
+         title="Location of the errors")
+p1
 
 #### On the other hand, the same CM inspection shows that is almost block diagonal,
 #### therefore this model could be much better in predicting only the building. 
@@ -250,37 +265,34 @@ trainingData$BFPred <- sapply(Highest_loc_train,
 #### In order to do this, we only need to get the first digit of the predicted
 #### building-floor.
 
-# building prediction #Accuracy: 0.9925
-BPrediction <- BFPrediction
-BPrediction$location <- str_sub(BPrediction$location, start = 1, end = -3)
-BPrediction$BUILDINGID_FLOOR <- str_sub(BPrediction$BUILDINGID_FLOOR, start = 1, end = -3)
+#### building prediction #Accuracy: 0.97 Kappa: 0.96 on the training set.
+#### By simple inspection of the Confussion Matrix, it can be seen that most
+#### of the mistakes made by this simple model takes place in the second building
+#### which is between the others. Signal receptions from WAPs of the others buildings
+#### confuses the model.
+Highest_loc_train$BLocation   <- str_sub(Highest_loc_train$BFLocation, start = 1, end = 1)
+Highest_loc_train$BPrediction <- str_sub(Highest_loc_train$Prediction, start = 1, end = 1)
 
-confusionMatrix(table(BPrediction$BUILDINGID_FLOOR, BPrediction$location))
+Highest_loc_train$BLocation   <- as.factor(Highest_loc_train$BLocation)
+Highest_loc_train$BPrediction <- as.factor(Highest_loc_train$BPrediction)
+
+BPredic_cm <- confusionMatrix(Highest_loc_train$BLocation,Highest_loc_train$BPrediction)
+
 
 # explore waps that affect multiple buildings #12
-BPrediction$Error <- BPrediction$location == BPrediction$BUILDINGID_FLOOR
-BPredictionErrors <- BPrediction[BPrediction$Error == FALSE, ]
-unique(BPredictionErrors$WAP)
-BErrors <- unique(BPrediction[BPrediction$Error == FALSE, ]$WAP)
-Data_T0_BE <- Data_T0[, !(colnames(Data_T0) %in% BErrors), drop = FALSE]
+#### Ignacio: The rigth question is, which waps are being detected in several buildings
+#### and where? Those WAPs should be removed in order to avoid confusing the models
+#### Another interesting question is why this happens.
+Highest_loc_train$BFCorrect <- Highest_loc_train$BFLocation == Highest_loc_train$Prediction
+BadWAPs <- unique(Highest_loc_train[which(Highest_loc_train$BFCorrect==FALSE),]$WAP)
+trainingData   <- trainingData[,!(colnames(trainingData) %in% BadWAPs), drop = FALSE]
+validationData <- validationData[,!(colnames(validationData) %in% BadWAPs), drop = FALSE]
 
-Data_T1 <- cbind(Data_T1, Data_T0[, c("LONGITUDE", "LATITUDE", "FLOOR", "BUILDINGID", "SPACEID",
-                                              "RELATIVEPOSITION", "USERID", "PHONEID", "TIMESTAMP")])
-
-for (i in BErrors) {
-  x <- Data_T1[, i]
-  plot(Data_T1$BUILDINGID_FLOOR, x, ylim = c(-110, 5))
-}
-
-for (i in BErrors) {
-  x <- Data_T1[, i]
-  plot(density(x, na.rm = TRUE))
-}
-
-rm("HighestWap", "BFLocation", "BFPrediction", "BFPredictionErrors", "BPrediction",
-   "BPredictionErrors", "Data_T1", "x", "i")
+Highest_loc_train <- Highest_loc_train[which(Highest_loc_train$BFCorrect==TRUE),]
 
 # phones ####
+#### Ignacio: Phone signal analysis. This is no longer needed as you already 
+#### fixed the WAPs signal strengths just cutting of the d
 # phones with signal 0 #phones 19 and 23 have 137 signals
 WAPs <- grep("WAP", names(trainingData), value=T)
 rowsw0 <- which(trainingData[, c(WAPs)] == 0, arr.ind=TRUE)
