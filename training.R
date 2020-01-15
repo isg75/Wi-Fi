b181904e2788269b1a00b5abe5e038595af0beb9
Ignacio <ignacio@ubiqum.com>
2020-01-15 15:44:34 +0100

    Last day of corrections to training.R

--- training.R
+++ training.R
@@ -88,8 +88,8 @@ p
 #### Ignacio: Avoid making copies and copies of your datasets, specially when they
 #### are big.
 
-trainingData[trainingData == 100] <- NA
-validationData[validationData == 100] <- NA
+#trainingData[trainingData == 100] <- NA
+#validationData[validationData == 100] <- NA
 
 #### Ignacio: Explain better what you are doing and why. You are trying yo figure
 #### out in which locations the user doesn't have any WIFI signal. In those locations
@@ -104,7 +104,6 @@ drop_allna_rows <- function(df) {
   df <- df[!ind,]
   return(df)
 }
-
 
 trainingData   <- drop_allna_rows(trainingData)
 validationData <- drop_allna_rows(validationData)
@@ -159,7 +158,7 @@ p0
 #### Feature engenieering ###
 #### Ignacio: Obtaning the signal value for which the density is highest.
 #### Moreover, why do you pick the value of "a" based on the training set?
-a <- density(dat1$value, na.rm = TRUE)$x[which.max(density(dat1$value, na.rm = TRUE)$y)]
+#a <- density(dat1$value, na.rm = TRUE)$x[which.max(density(dat1$value, na.rm = TRUE)$y)]
 
 #### Ignacio: replacing values smaller than a by a. Ferran, what is the idea behind
 #### this approach? This should be explained. At the same time, you are replacing 
@@ -172,8 +171,11 @@ a <- density(dat1$value, na.rm = TRUE)$x[which.max(density(dat1$value, na.rm = T
 #### See: https://support.randomsolutions.nl/827069-Best-dBm-Values-for-Wifi
 
 replace_values <- function(x) {
-  ifelse(x < -90,x<- -90,ifelse(x > -30,x <- -30, x <- x))
-}
+  if ( x != 100) {
+    return(ifelse(x < -90,x<- -90,ifelse(x > -30,x <- -30, x <- x)))
+  }
+}  
+
 
 trainingData[,c(WAPs)]   <- mapply(replace_values,trainingData[,c(WAPs)])
 validationData[,c(WAPs)] <- mapply(replace_values,validationData[,c(WAPs)])
@@ -231,8 +233,8 @@ Highest_loc_train$Prediction <- sapply(Highest_loc_train$WAP,function(x) multipl
 
 #### Getting the confussion Matrix of the model on the training set ####
 BFPred_cm <- confusionMatrix(trainingData$BUILDINGID_FLOOR, Highest_loc_train$Prediction)
-#### As it can be seen, the accuracy is pretty small: 0.53, while the kappa
-#### is 0.48. Therefore this model can be used as a benchmark, but not for
+#### As it can be seen, the accuracy is pretty small: 0.54, while the kappa
+#### is 0.50. Therefore this model can only be used as a benchmark, but not for
 #### production.
 #### Close inspection of the Confusion Matrix (CM henceforth) shows that some
 #### waps sometimes give the strongest signal in two different buildings. 
@@ -276,6 +278,9 @@ Highest_loc_train$BPrediction <- str_sub(Highest_loc_train$Prediction, start = 1
 Highest_loc_train$BLocation   <- as.factor(Highest_loc_train$BLocation)
 Highest_loc_train$BPrediction <- as.factor(Highest_loc_train$BPrediction)
 
+#### As it could be expected, this new modelhas better accuracy and kappa on the
+#### training set.
+#### Accuracy: 0.97, Kappa: 0.96
 BPredic_cm <- confusionMatrix(Highest_loc_train$BLocation,Highest_loc_train$BPrediction)
 
 
@@ -290,182 +295,16 @@ validationData <- validationData[,!(colnames(validationData) %in% BadWAPs), drop
 
 Highest_loc_train <- Highest_loc_train[which(Highest_loc_train$BFCorrect==TRUE),]
 
-# phones ####
-#### Ignacio: Phone signal analysis. This is no longer needed as you already 
-#### fixed the WAPs signal strengths just cutting of the d
-# phones with signal 0 #phones 19 and 23 have 137 signals
-WAPs <- grep("WAP", names(trainingData), value=T)
-rowsw0 <- which(trainingData[, c(WAPs)] == 0, arr.ind=TRUE)
-Data_T0test <- trainingData[rowsw0, ]
-
-table(Data_T0test$PHONEID)
-
-# phones with signal stronger than -30 #phones 19 and 23 have have 700+ signals
-rowsw30 <- which(trainingData[, c(WAPs)] > -30, arr.ind=TRUE)
-
-Data_T0test <- Data_T0[rowsw30, ]
-
-table(Data_T0test$PHONEID)
-
-# phones with signal weaker than -90 #phone 23 have 55.000+ signals, phone 13 have 15.000+ signals
-rowsw90 <- which(trainingData[, c(WAPs)] < -90, arr.ind=TRUE)
-
-Data_T0test <- Data_T0[rowsw90, ]
-
-table(Data_T0test$PHONEID)
-
-# remove rows with a signal of 0 #worse
-#rowsw0 <- as.data.frame(rowsw0)
-
-#Data_T0 <- Data_T0[-rowsw0$row, ]
-#Data_T0_BE <- Data_T0_BE[-rowsw0$row, ]
-#Data_T0_BFE <- Data_T0_BFE[-rowsw0$row, ]
-
-# remove phone 23 #worse
-trainingData[is.na(trainingData)] <- -100
-
-#Data_T0 <- subset(Data_T0, !PHONEID == "23")
-#Data_T0_BE <- subset(Data_T0_BE, !PHONEID == "23")
-#Data_T0_BFE <- subset(Data_T0_BFE, !PHONEID == "23")
-
-rm("rowsw0", "rowsw30", "rowsw90", "Data_T0test")
-
-# top 3 waps ####
-# top 3 waps for each observation #276
-Data_T0_T3W <- subset(Data_T0, select = -c(LONGITUDE, LATITUDE, BUILDINGID_FLOOR, FLOOR, BUILDINGID,
-                                           SPACEID, RELATIVEPOSITION, USERID, PHONEID, TIMESTAMP))
-
-Data_T0_T3W$Top3Waps <- apply(Data_T0_T3W, 1, function(x) 
+trainingData$Top3Waps <- apply(trainingData[,c(WAPs)], 1, function(x) 
   paste(names(head(sort(x, decreasing = TRUE), 3)), collapse = " "))
 
-Data_T0_T3W <- separate(Data_T0_T3W, Top3Waps, c("TopWap1", "TopWap2","TopWap3"))
+trainingData <- separate(trainingData, Top3Waps, c("TopWap1", "TopWap2","TopWap3"))
 
-T3W <- unique(c(Data_T0_T3W$TopWap1, Data_T0_T3W$TopWap2, Data_T0_T3W$TopWap3))
-Data_T0_T3W <- Data_T0[, T3W]
-Data_T0_T3W <- cbind(Data_T0_T3W, Data_T0[, c("LONGITUDE", "LATITUDE", "FLOOR", "BUILDINGID", "SPACEID",
-                                                "RELATIVEPOSITION", "USERID", "PHONEID", "TIMESTAMP",
-                                                "BUILDINGID_FLOOR")])
+trainingData$TopWap1 <- factor(trainingData$TopWap1, levels = WAPs)
+trainingData$TopWap2 <- factor(trainingData$TopWap2, levels = WAPs)
+trainingData$TopWap3 <- factor(trainingData$TopWap3, levels = WAPs)
 
-rm("T3W")
-
-# top 3 waps (without BF Errors) for each observation #223
-Data_T0_T3W_BFE <- subset(Data_T0, select = -c(LONGITUDE, LATITUDE, BUILDINGID_FLOOR, FLOOR, BUILDINGID,
-                                               SPACEID, RELATIVEPOSITION, USERID, PHONEID, TIMESTAMP))
-Data_T0_T3W_BFE <- Data_T0_T3W_BFE[, !(colnames(Data_T0_T3W_BFE) %in% BFErrors), drop = FALSE]
-
-Data_T0_T3W_BFE$Top3Waps <- apply(Data_T0_T3W_BFE, 1, function(x) 
-  paste(names(head(sort(x, decreasing = TRUE), 3)), collapse = " "))
-
-Data_T0_T3W_BFE <- separate(Data_T0_T3W_BFE, Top3Waps, c("TopWap1", "TopWap2","TopWap3"))
-
-T3W_BFE <- unique(c(Data_T0_T3W_BFE$TopWap1, Data_T0_T3W_BFE$TopWap2, Data_T0_T3W_BFE$TopWap3))
-Data_T0_T3W_BFE <- Data_T0[, T3W_BFE]
-Data_T0_T3W_BFE <- cbind(Data_T0_T3W_BFE, Data_T0[, c("LONGITUDE", "LATITUDE", "FLOOR", "BUILDINGID",
-                                                      "SPACEID", "RELATIVEPOSITION", "USERID",
-                                                      "PHONEID", "TIMESTAMP", "BUILDINGID_FLOOR")])
-
-rm("T3W_BFE")
-
-# top 3 waps (without B Errors) for each observation #267
-Data_T0_T3W_BE <- subset(Data_T0, select = -c(LONGITUDE, LATITUDE, BUILDINGID_FLOOR, FLOOR, BUILDINGID,
-                                              SPACEID, RELATIVEPOSITION, USERID, PHONEID, TIMESTAMP))
-Data_T0_T3W_BE <- Data_T0_T3W_BE[, !(colnames(Data_T0_T3W_BE) %in% BErrors), drop = FALSE]
-
-Data_T0_T3W_BE$Top3Waps <- apply(Data_T0_T3W_BE, 1, function(x) 
-  paste(names(head(sort(x, decreasing = TRUE), 3)), collapse = " "))
-
-Data_T0_T3W_BE <- separate(Data_T0_T3W_BE, Top3Waps, c("TopWap1", "TopWap2","TopWap3"))
-
-T3W_BE <- unique(c(Data_T0_T3W_BE$TopWap1, Data_T0_T3W_BE$TopWap2, Data_T0_T3W_BE$TopWap3))
-Data_T0_T3W_BE <- Data_T0[, T3W_BE]
-Data_T0_T3W_BE <- cbind(Data_T0_T3W_BE, Data_T0[, c("LONGITUDE", "LATITUDE", "FLOOR", "BUILDINGID",
-                                                    "SPACEID", "RELATIVEPOSITION", "USERID", "PHONEID",
-                                                    "TIMESTAMP", "BUILDINGID_FLOOR")])
-
-rm("T3W_BE")
-
-# top 1 wap
-# top 1 wap for each observation #246
-Data_T0_T1W <- subset(Data_T0, select = -c(LONGITUDE, LATITUDE, BUILDINGID_FLOOR, FLOOR, BUILDINGID,
-                                           SPACEID, RELATIVEPOSITION, USERID, PHONEID, TIMESTAMP))
-
-Data_T0_T1W$Top1Waps <- apply(Data_T0_T1W, 1, function(x) 
-  paste(names(head(sort(x, decreasing = TRUE), 1)), collapse = " "))
-
-T1W <- unique(Data_T0_T1W$Top1Waps)
-Data_T0_T1W <- Data_T0[, T1W]
-Data_T0_T1W <- cbind(Data_T0_T1W, Data_T0[, c("LONGITUDE", "LATITUDE", "FLOOR", "BUILDINGID", "SPACEID",
-                                              "RELATIVEPOSITION", "USERID", "PHONEID", "TIMESTAMP",
-                                              "BUILDINGID_FLOOR")])
-
-rm("T1W")
-
-# top 1 wap (without BF Errors) for each observation #152
-Data_T0_T1W_BFE <- subset(Data_T0, select = -c(LONGITUDE, LATITUDE, BUILDINGID_FLOOR, FLOOR, BUILDINGID,
-                                               SPACEID, RELATIVEPOSITION, USERID, PHONEID, TIMESTAMP))
-Data_T0_T1W_BFE <- Data_T0_T1W_BFE[, !(colnames(Data_T0_T1W_BFE) %in% BFErrors), drop = FALSE]
-
-Data_T0_T1W_BFE$Top1Waps <- apply(Data_T0_T1W_BFE, 1, function(x) 
-  paste(names(head(sort(x, decreasing = TRUE), 1)), collapse = " "))
-
-T1W_BFE <- unique(Data_T0_T1W_BFE$Top1Waps)
-Data_T0_T1W_BFE <- Data_T0[, T1W_BFE]
-Data_T0_T1W_BFE <- cbind(Data_T0_T1W_BFE, Data_T0[, c("LONGITUDE", "LATITUDE", "FLOOR", "BUILDINGID",
-                                                      "SPACEID", "RELATIVEPOSITION", "USERID",
-                                                      "PHONEID", "TIMESTAMP", "BUILDINGID_FLOOR")])
-
-rm("T1W_BFE")
-
-# top 1 wap (without B Errors) for each observation #237
-Data_T0_T1W_BE <- subset(Data_T0, select = -c(LONGITUDE, LATITUDE, BUILDINGID_FLOOR, FLOOR, BUILDINGID,
-                                              SPACEID, RELATIVEPOSITION, USERID, PHONEID, TIMESTAMP))
-Data_T0_T1W_BE <- Data_T0_T1W_BE[, !(colnames(Data_T0_T1W_BE) %in% BErrors), drop = FALSE]
-
-Data_T0_T1W_BE$Top1Waps <- apply(Data_T0_T1W_BE, 1, function(x) 
-  paste(names(head(sort(x, decreasing = TRUE), 1)), collapse = " "))
-
-T1W_BE <- unique(Data_T0_T1W_BE$Top1Waps)
-Data_T0_T1W_BE <- Data_T0[, T1W_BE]
-Data_T0_T1W_BE <- cbind(Data_T0_T1W_BE, Data_T0[, c("LONGITUDE", "LATITUDE", "FLOOR", "BUILDINGID",
-                                                    "SPACEID", "RELATIVEPOSITION", "USERID", "PHONEID",
-                                                    "TIMESTAMP", "BUILDINGID_FLOOR")])
-
-rm("T1W_BE")
-rm("BErrors", "BFErrors", "WAPs")
-gc()
-
+trainingData[trainingData == 100] <- -100
 validationData[validationData == 100] <- -100
-
-
-#normalize test ## didnt work ##
-#Data_T0[Data_T0 == -100] <- NA
-#WAPs <- grep("WAP", names(Data_T0), value=T)
-#Data_T01 <- Data_T0[, WAPs]
-#Data_T01 <- abs(Data_T01)
-#Data_T01 <- as.data.frame(t(apply(Data_T01, 1, function(x, ...)(x - min(x, ...))/(max(x, ...) - min(x, ...)))))
-
-#Data_T0[is.na(Data_T0)] <- -100
-
-# test set pre processing for final test ##
-#validationData <- read_csv("C:/Users/FDL_4/OneDrive/Escritorio/Course/Module 4/Task 1/UJIndoorLoc/Copy of Copy of testData - Copy of Copy of testData.csv")
-#View(validationData)
-#str(validationData[520:529])
-
-#validationData$FLOOR <- as.factor(validationData$FLOOR)
-#validationData$BUILDINGID <- as.factor(validationData$BUILDINGID)
-#validationData$SPACEID <- as.factor(validationData$SPACEID)
-#validationData$RELATIVEPOSITION <- as.factor(validationData$RELATIVEPOSITION)
-#validationData$USERID <- as.factor(validationData$USERID)
-#validationData$PHONEID <- as.factor(validationData$PHONEID)
-#validationData$TIMESTAMP <- as_datetime(validationData$TIMESTAMP)
-
-#WAPs <- grep("WAP", names(validationData), value=T)
-#validationData1 <- validationData[, WAPs]
-#validationData1[validationData1 == 100] <- NA
-#validationData1[validationData1 < a] <- a
-#validationData1[validationData1 > -29] <- -29
-#validationData <- cbind(validationData1, validationData[, c("LONGITUDE", "LATITUDE", "FLOOR", "BUILDINGID", "SPACEID",
-#                                                            "RELATIVEPOSITION", "USERID", "PHONEID", "TIMESTAMP")])
-#validationData[is.na(validationData)] <- -100
 
 
